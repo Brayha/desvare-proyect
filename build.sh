3e74a81cee947f69a472eb0ea237fe64564f7a21
Brayan Farid Garcia Garzon <bgarcia@MacBook-Air-de-Brayan.local>
2026-02-05 16:43:26 -0500

    fix: Instalar dependencias de shared en build.sh para Client PWA

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
+echo "📦 Installing dependencies in shared..."
+cd shared
+npm install
+
 echo "📦 Installing dependencies in client-pwa..."
-cd client-pwa
+cd ../client-pwa
 npm install
 
 echo "🔨 Building client-pwa..."
